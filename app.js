9cd83fb3b05b5b487b86e2d5500dfe8063afc171
Constanza <constanza.luz.gonzalez@gmail.com>
2020-09-24 13:06:32 -0300

    Clase2

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 (() => {
-    Empleado = require('./empleado');
-    let empleadito =  new Empleado('Juancito', 21);
-    empleadito.saludo();
-})();
 
-(() => {
-    fs = require('fs');
-    let archivo = fs.readFileSync(__dirname + '/texto.txt', 'utf8')
-    console.log(archivo)
-})();
+const http = require('http');
+const fs = require('fs');
 
-(() => {
-    fs = require('fs');
-    fs.readFile(__dirname + '/texto.txt', 'utf8', (err, data) => {
-        console.log(data);
-    })
-})();
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        console.log("Buenas buenas");
+        const html = fs.readFile('./index.html')
+        res.setHeader('Content-Type', 'text/html');
+        res.write(html);
+    }
+    else {
+        res.end('La pagina no existe');
+    }
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log('Servidor escuchando...'), 3000;
+})
+
+})()
